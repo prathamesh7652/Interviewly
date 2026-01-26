90f48c627005638ecc3cadd3db5565caf450941d
prathamesh7652 <nprathamesh7652@gmail.com>
2026-01-26 20:43:14 +0530

    feat: Set up initial monorepo project structure with basic frontend and backend applications including dependency management and server configuration.

--- Backend/src/lib/env.js
+++ Backend/src/lib/env.js
@@ -4,7 +4,8 @@ dotenv.config();
 
 const ENV = {
     PORT : process.env.PORT,
-    DB_URL : process.env.DB_URL
+    DB_URL : process.env.DB_URL,
+    NODE_ENV : process.env.NODE_ENV
 
 }
 
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,13 +1,33 @@
 import express from "express"
 import ENV from "./lib/env.js";
+import path from "path"
 
 const app =  express ();
 
+const ___dirname = path.resolve();
 
 app.get ("/" , (req,res) => {
+    res.status(200).json({msg: "main"})
+});
+app.get ("/health" , (req,res) => {
+    res.status(200).json({msg: "health"})
+});
+app.get ("/box" , (req,res) => {
     res.status(200).json({msg: "success from backend"})
 });
 
 
-app.listen(ENV.PORT, () => console.log("Server Is Running On Port:",ENV.PORT))
 
+// If we are in Production
+
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(___dirname, "../frontend/dist")))
+
+    app.get("/{*any}" , (req,res) =>{
+        res.sendFile(path.join(___dirname, "../frontend" ,"dist" ,"index.html"))
+    } )
+}
+
+
+
+app.listen(ENV.PORT, () => console.log("Server Is Running On Port:",ENV.PORT))
