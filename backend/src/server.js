8040b322e8f01887381f221ca1ca2c23a6010bb5
prathamesh7652 <nprathamesh7652@gmail.com>
2026-01-29 17:31:54 +0530

    server-side-auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,21 +5,24 @@ import connectDB from "./lib/db.js";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
+import { clerkMiddleware } from "@clerk/express";
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 const __dirname = path.resolve();
 
 // middleware
 app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-// Inngest endpoint
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
 
-app.get("/api", (req, res) => {
-  res.status(200).json({ msg: "main" });
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
-
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "health" });
 });
